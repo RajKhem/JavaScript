650097de4bd06a90a720dd6c9765faf0bbec6668
Khem Raj Paneru <86644220+RajKhem@users.noreply.github.com>
2024-01-26 08:24:45 +0545

    About the JS console
    Printing first code in JS

---
+++ 00console.js
@@ -0,0 +1,13 @@
+/*
+Using the console:
+It uses REPL ("READ-EVALUATE-PRINT-LOOP") mechanism, which means:
+ 1.Firstly it reads the input or given statement then,
+ 2.It calculates the given input computes it then,
+ 3.It prints the result. 
+ 4.If any itiration or loop exists then it repeats the above three steps.
+*/
+
+console.log("WELCOME TO JAVASCRIPT JOURNEY");
+/*simply it prints the output as:
+ WELCOME TO JAVASCRIPT JOURNEY
+*/
