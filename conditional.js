e1baffa67bdfed2c4c6055b46c8cf5571c1fecb7
Khem Raj Paneru <86644220+RajKhem@users.noreply.github.com>
2024-02-26 21:00:31 +0545

    conditionalStatements in javaScript

---
+++ conditional.js
@@ -0,0 +1,66 @@
+//conditional statement
+
+                // 1 IF STATEMENT
+// let age = 16;
+
+//  if(age>=18){
+//     console.log("you can vote");
+//  }
+
+//  if(age <18){
+//     console.log("you cannot vote");
+//  }
+
+//  let mode="dark";
+//  let color;
+
+//  if (mode === "dark"){
+//     color = "black";
+//  }
+ 
+//  if (mode === "light"){
+//     color = "white";
+//  }
+//  console.log(color);
+
+
+                // 2 IF-ELSE STATEMENT
+
+//  let mode="blue";
+//  let color;
+
+//  if (mode === "blue"){
+//     color = "black";
+//  }
+// else{
+//     color = "white";
+//  }
+//  console.log(color);
+
+            //voting condition
+                //  let age = 16;
+                //  if(age>=18){
+                //     console.log("you can vote");}
+                //  else
+                //  {
+                //     console.log("you cannot vote");
+                //  }
+
+
+        // odd or even
+        // let num = 19;
+        // if (num % 2 === 0){
+        //     console.log(num," is even number");
+        // }
+        // else{
+        //     console.log(num," is odd number");
+        // }
+
+
+
+                 // 3 ELSE IF STATEMENT     
+         
+                 
+
+
+                 
